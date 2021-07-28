02a7659be152f1c8130100e48620e5c65c982444
Tushar <Tushar@Tushar.mshome.net>
2021-07-28 10:06:31 +0530

    HB Generators-XML

--- 29-HB-Generators/src/main/java/com/tushar/main/GeneratorTest.java
+++ 29-HB-Generators/src/main/java/com/tushar/main/GeneratorTest.java
@@ -8,13 +8,13 @@ import org.hibernate.Transaction;
 import com.tushar.model.Product;
 import com.tushar.utility.HBConnection_improved;
 
-public class assignedGenerator{
+public class GeneratorTest{
 
 	public static void main(String[] args) {
 		
 		Transaction tX =null;
 		
-		System.out.println("assignedGenerator.main() STARTED");
+		System.out.println("GeneratorTest.main() STARTED");
 		
 		try(SessionFactory factory =HBConnection_improved.getSessionFactory();
 				Session ses = HBConnection_improved.getSession();
@@ -29,6 +29,34 @@ public class assignedGenerator{
 			 */
 			
 			/*for <generator class="identity"></generator>
+			 *  no need to specify id Val. Suitable for MultiThreaded env.  &  Clustered env also
+			 */
+			
+			/*for <generator class="sequence">
+						<param name="sequence_name">PROD_ID_SEQ</param>
+						<param name="initial_value">41</param>
+						<param name="increment_size">1</param>
+					</generator>
+			 *  no need to specify id Val. Suitable for MultiThreaded env.  &  Clustered env also
+			 *  internally it will create new sequence in ORACLE DB and in MYSQL it creates table to store/retrieve sequence value
+			 *  ORACLE:: Hibernate: create sequence PROD_ID_SEQ start with 41 increment by  1
+			 *  MYSQL:: create table PROD_ID_SEQ ( next_val bigint  )
+			 */
+			
+			
+			/*
+			 * HILO generator runs upto HIbernate4.x only
+			 * <!-- https://mvnrepository.com/artifact/org.hibernate/hibernate-core -->
+				<dependency>
+					<groupId>org.hibernate</groupId>
+					<artifactId>hibernate-core</artifactId>
+					<version>4.3.11.Final</version>
+				</dependency>
+			 * <generator class="hilo"> <param
+			 * name="table">PROD_ID_HILO</param> 
+			 * <param name="column">HI_VAL</param> 
+			 * <param  name="max_lo">2</param> 
+			 * </generator>
 			 *  no need to specify id Val. Suitable for MultiThreaded env.  &  Clustered env also
 			 */
 			
