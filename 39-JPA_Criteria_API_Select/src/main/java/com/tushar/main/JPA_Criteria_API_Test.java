a23174fab335853343edcf754c140ffd34fcdd02
Tushar <Tushar@Tushar.mshome.net>
2021-08-21 14:32:38 +0530

    HB Criteria API ::: select, aggregate function

--- 39-JPA_Criteria_API_Select/src/main/java/com/tushar/main/JPA_Criteria_API_Test.java
+++ 39-JPA_Criteria_API_Select/src/main/java/com/tushar/main/JPA_Criteria_API_Test.java
@@ -72,6 +72,40 @@ public class JPA_Criteria_API_Test {
 			Query query4=ses.createQuery(cQuery4);
 			List<Product> list4 = query4.getResultList();
 			list4.forEach(System.out::println);
+			
+			System.out.println("--------SCALAR------------SELECT specific-----------------");
+			CriteriaBuilder cBuilder5 =ses.getCriteriaBuilder();
+			CriteriaQuery<Object[]> cQuery5 = cBuilder5.createQuery(Object[].class);
+			Root rootProd5 = cQuery5.from(Product.class);
+			cQuery5.multiselect(rootProd5.get("PID"),rootProd5.get("prodname"),rootProd5.get("price"));
+			Query query5=ses.createQuery(cQuery5);
+			List<Object[]> list5 = query5.getResultList();
+			list5.forEach( prod->{
+				for(Object val : prod)
+					System.out.print(val+"  ");
+				System.out.println();
+			});
+			
+			
+			System.out.println("--------SCALAR------------SELECT aggregate functions-----------------");
+			CriteriaBuilder cBuilder6 =ses.getCriteriaBuilder();
+			CriteriaQuery<Object[]> cQuery6 = cBuilder6.createQuery(Object[].class);
+			Root rootProd6 = cQuery6.from(Product.class);
+			cQuery6.multiselect( 
+					cBuilder6.count(rootProd6.get("PID")),   
+					cBuilder6.sum(rootProd6.get("PID")) ,   
+					cBuilder6.avg(rootProd6.get("PID")), 
+					cBuilder6.min(rootProd6.get("PID")),
+					cBuilder6.max(rootProd6.get("PID"))
+					);
+			Query query6=ses.createQuery(cQuery6);
+			List<Object[]> list6 = query6.getResultList();
+			Object[] data =list6.get(0);
+			System.out.println("count   #"+data[0]);
+			System.out.println("sum   #"+data[1]);
+			System.out.println("avg   #"+data[2]);
+			System.out.println("min   #"+data[3]);
+			System.out.println("max   #"+data[4]);
 			 
 		} catch (HibernateException e) {
 			System.out.println("Error while performing operation");
