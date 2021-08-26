c0475c13f76d6436a31437e3eff9312d222ccee0
Tushar <Tushar@Tushar.mshome.net>
2021-08-26 08:57:47 +0530

    Pagination using: HQL, JPQL, HB Criteria API, JPA Criteria API

--- 41-Pagination_HQL_JPQL/src/main/java/com/tushar/main/Pagination_Test.java
+++ 41-Pagination_HQL_JPQL/src/main/java/com/tushar/main/Pagination_Test.java
@@ -9,10 +9,13 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.CriteriaUpdate;
 import javax.persistence.criteria.Root;
 
+import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Projection;
+import org.hibernate.criterion.Projections;
 import org.hibernate.query.NativeQuery;
 import org.hibernate.type.StandardBasicTypes;
 import org.hibernate.type.Type;
@@ -89,6 +92,51 @@ public class Pagination_Test {
 					e.printStackTrace();
 				}
 			}
+			
+			System.out.println("**************************HB Criteria API_Pagination_Test STARTED **************************");
+			Criteria hbCriteriaCount = ses.createCriteria(Product.class);
+			Projection pCount = Projections.count("PID");
+			hbCriteriaCount.setProjection(pCount);
+			long hbCriteriaRowCount = (long) hbCriteriaCount.list().get(0);
+			
+			int hbPageSize =3;
+			long pageCount_hbCriteria  = hbCriteriaRowCount/hbPageSize;
+			pageCount_hbCriteria =    (hbCriteriaRowCount%pageCount_hbCriteria==0 ) ? pageCount_hbCriteria:  pageCount_hbCriteria++;
+			
+			Criteria hbQueryPage =  ses.createCriteria(Product.class);
+			
+			for(int i =0, j=1; i<hbCriteriaRowCount;i+=hbPageSize, j++) {
+			hbQueryPage.setFirstResult(i);
+			hbQueryPage.setMaxResults(hbPageSize);
+			List<Product> hbCriteriaProdList =  hbQueryPage.list();
+			System.out.println("*HB Criteria API_Page: "+ j);	
+			hbCriteriaProdList.forEach(System.out::println);
+			}
+			
+			System.out.println("**************************JPA Criteria API_Pagination_Test STARTED **************************");
+			CriteriaBuilder jpaCriteriaBuilder = ses.getCriteriaBuilder();
+			CriteriaQuery  jpaCriteriaQuery =jpaCriteriaBuilder.createQuery();
+			Root<Product>  jpaRoot =  jpaCriteriaQuery.from(Product.class);
+			jpaCriteriaQuery.multiselect(jpaCriteriaBuilder.count(jpaRoot.get("PID")));
+			Query jpaQuery = ses.createQuery(jpaCriteriaQuery);
+			long jpaCriteriaRowCount = (long) jpaQuery.getSingleResult();
+			
+			int jpaPageSize =3;
+			long pageCount_jpaCriteria  = jpaCriteriaRowCount/jpaPageSize;
+			pageCount_jpaCriteria =    (jpaCriteriaRowCount%pageCount_jpaCriteria==0 ) ? pageCount_jpaCriteria:  pageCount_jpaCriteria++;
+			
+			CriteriaQuery  jpaCriteriaQuery1 =jpaCriteriaBuilder.createQuery();
+			Root<Product>  jpaRoot1 =  jpaCriteriaQuery1.from(Product.class);
+			jpaCriteriaQuery1.select(jpaRoot1);
+			Query jpaQuery1 = ses.createQuery(jpaCriteriaQuery1);
+			
+			for(int i =0, j=1; i<jpaCriteriaRowCount;i+=jpaPageSize, j++) {
+				jpaQuery1.setFirstResult(i);
+				jpaQuery1.setMaxResults(jpaPageSize);
+				List<Product> jpaCriteriaProdList =  jpaQuery1.getResultList();
+				System.out.println("*JPA Criteria API_Page: "+ j);	
+				jpaCriteriaProdList.forEach(System.out::println);
+				}
 			
 			
 		}//try
