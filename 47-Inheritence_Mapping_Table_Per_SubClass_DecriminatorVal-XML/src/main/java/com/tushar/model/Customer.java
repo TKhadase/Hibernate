a284f12019e9e8af5e77ede9882f6e91318fdf74
Tushar <Tushar@Tushar.mshome.net>
2021-09-01 22:35:45 +0530

    Inheritence_Mapping_Table_Per_SubClass with Decriminator Value  : XML

---
+++ 47-Inheritence_Mapping_Table_Per_SubClass_DecriminatorVal-XML/src/main/java/com/tushar/model/Customer.java
@@ -0,0 +1,26 @@
+package com.tushar.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer extends Person {
+	
+	private double billAmt;
+	private String billDate;
+	private String paymentType;
+	private String billingLocation;
+	
+	@Override
+	public String toString() {
+		return "Customer [billAmt=" + billAmt + ", billDate=" + billDate + ", paymentType=" + paymentType + ", billingLocation="
+				+ billingLocation + ",  PID=" + getPID() + ", fname="
+				+ getFname() + ", lname=" + getLname() + ", addrs=" + getAddrs() + "]";
+	}
+	
+}
